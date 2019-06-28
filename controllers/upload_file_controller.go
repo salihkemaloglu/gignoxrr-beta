c9cb2549ed45d35e3fa0065e48916d7c29265a31
salihkemaloglu <salihkemaloglu@gmail.com>
2019-06-28 12:19:51 +0300

    add file upload

---
+++ controllers/upload_file_controller.go
@@ -0,0 +1,44 @@
+package controller
+import (
+	"fmt"
+	"io"
+	"os"
+	"log"
+	"bytes"
+	"path/filepath"
+	"github.com/salihkemaloglu/gignox-rr-beta-001/proto"
+
+)
+
+func UploadFileController(stream  gigxRR.GigxRRService_UploadFileServer) error{
+	for {
+		req, err := stream.Recv()
+		if err == io.EOF {
+			// we have finished reading the client stream
+			return nil
+		}
+		if err != nil {
+			log.Fatalf("Error while reading client stream: %v", err)
+		}
+		file := req.GetFile()
+		// copy example
+		absPath, _ := filepath.Abs("handlersss.mp4")
+		f, err := os.OpenFile(absPath, os.O_WRONLY|os.O_CREATE, 0666)
+		if err != nil {
+			fmt.Println(err)
+
+		}
+		defer f.Close() //if there is a bug. If the call to os.Create fails, the function will return without closing the source file. defer is closing it.
+		reader := bytes.NewReader(file)
+		io.Copy(f, reader)
+
+		sendErr := stream.Send(&gigxRR.UploadFileResponse{
+			Result: "ok",
+		})
+		if sendErr != nil {
+			fmt.Println("Error while sending data to client:", sendErr)
+			return sendErr
+		}
+		fmt.Printf("LongGreet function was close with a streaming request\n")
+	}
+}
--- main.go
+++ main.go
@@ -1,20 +1,20 @@
 package main
 
 import (
-	
-	"fmt"
-	"time"
 	"context"
+	"fmt"
 	"net/http"
+	"time"
+
+	"github.com/improbable-eng/grpc-web/go/grpcweb"
+	"github.com/patrickmn/go-cache"
+	cont "github.com/salihkemaloglu/gignox-rr-beta-001/controllers"
+	gigxRR "github.com/salihkemaloglu/gignox-rr-beta-001/proto"
+	repo "github.com/salihkemaloglu/gignox-rr-beta-001/repositories"
+	helper "github.com/salihkemaloglu/gignox-rr-beta-001/services"
 	"github.com/spf13/pflag"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/grpclog"
-	"github.com/patrickmn/go-cache"
-	"github.com/improbable-eng/grpc-web/go/grpcweb"
-	"github.com/salihkemaloglu/gignox-rr-beta-001/proto"
-	repo "github.com/salihkemaloglu/gignox-rr-beta-001/repositories"
-	helper "github.com/salihkemaloglu/gignox-rr-beta-001/services"
-	cont "github.com/salihkemaloglu/gignox-rr-beta-001/controllers"
 )
 
 type server struct {
@@ -23,12 +23,13 @@ type server struct {
 var (
 	c *cache.Cache
 	// useWebsockets = pflag.Bool("use_websockets", false, "whether to use beta websocket transport layer")
-	enableTls       = pflag.Bool("enable_tls", false, "Use TLS - required for HTTP2.")// false is for local development
+	enableTls       = pflag.Bool("enable_tls", false, "Use TLS - required for HTTP2.") // false is for local development
 	tlsCertFilePath = pflag.String("tls_cert_file", "app_root/ssl/fullchain.pem", "Path to the CRT/PEM file.")
 	tlsKeyFilePath  = pflag.String("tls_key_file", "app_root/ssl/privkey.pem", "Path to the private key file.")
 	// flagHttpMaxWriteTimeout = pflag.Duration("server_http_max_write_timeout", 10*time.Second, "HTTP server config, max write duration.")
 	// flagHttpMaxReadTimeout  = pflag.Duration("server_http_max_read_timeout", 10*time.Second, "HTTP server config, max read duration.")
 )
+
 func (s *server) SayHello(ctx context.Context, req *gigxRR.HelloRequest) (*gigxRR.HelloResponse, error) {
 
 	fmt.Printf("RR service is working for SayHello...Received rpc from client, message=%s\n", req.GetMessage())
@@ -40,63 +41,67 @@ func (s *server) GetIpInformation(ctx context.Context, req *gigxRR.GetIpInformat
 	return cont.GetIpInformationController(ctx)
 }
 func (s *server) Login(ctx context.Context, req *gigxRR.LoginUserRequest) (*gigxRR.LoginUserResponse, error) {
-	
+
 	fmt.Printf("RR service is working for Login...Received rpc from client.\n")
-	return cont.LoginController(ctx,req,c)
+	return cont.LoginController(ctx, req, c)
 }
 func (s *server) Register(ctx context.Context, req *gigxRR.RegisterUserRequest) (*gigxRR.RegisterUserResponse, error) {
-	
+
 	fmt.Printf("RR service is working for Register...Received rpc from client.\n")
-	return cont.RegisterController(ctx,req)	
+	return cont.RegisterController(ctx, req)
 }
 func (s *server) CheckUserToRegister(ctx context.Context, req *gigxRR.CheckUserToRegisterRequest) (*gigxRR.CheckUserToRegisterResponse, error) {
-	
+
 	fmt.Printf("RR service is working for CheckUserToRegister...Received rpc from client.\n")
-	return cont.CheckUserToRegisterController(ctx,req)	
+	return cont.CheckUserToRegisterController(ctx, req)
 }
 func (s *server) SendEmail(ctx context.Context, req *gigxRR.SendEmailRequest) (*gigxRR.SendEmailResponse, error) {
-	
+
 	fmt.Printf("RR service is working for SendMail...Received rpc from client.\n")
-	return cont.SendEmailController(ctx,req)		
+	return cont.SendEmailController(ctx, req)
 }
 func (s *server) CheckVerificationLink(ctx context.Context, req *gigxRR.CheckVerificationLinkRequest) (*gigxRR.CheckVerificationLinkResponse, error) {
 
 	fmt.Printf("RR service is working for CheckVerificationLink...Received rpc from client.\n")
-	return cont.CheckVerificationLinkController(ctx,req)	
+	return cont.CheckVerificationLinkController(ctx, req)
 }
 func (s *server) ResetUserPassword(ctx context.Context, req *gigxRR.ResetUserPasswordRequest) (*gigxRR.ResetUserPasswordResponse, error) {
 
 	fmt.Printf("RR service is working for ResetUserPassword...Received rpc from client.\n")
-	return cont.ResetUserPasswordController(ctx,req)	
+	return cont.ResetUserPasswordController(ctx, req)
 }
 func (s *server) GetUser(ctx context.Context, req *gigxRR.GetUserRequest) (*gigxRR.GetUserResponse, error) {
 
 	fmt.Printf("RR service is working for GetUser...Received rpc from client.\n")
-	return cont.GetUserController(ctx,req)	
+	return cont.GetUserController(ctx, req)
 }
 func (s *server) UpdateUser(ctx context.Context, req *gigxRR.UpdateUserRequest) (*gigxRR.UpdateUserResponse, error) {
-	return nil,nil
+	return nil, nil
 }
 func (s *server) DeleteUser(ctx context.Context, req *gigxRR.DeleteUserRequest) (*gigxRR.DeleteUserResponse, error) {
-	return nil,nil
+	return nil, nil
 }
 func (s *server) GetFile(ctx context.Context, req *gigxRR.GetFileRequest) (*gigxRR.GetFileResponse, error) {
-	return nil,nil
+	return nil, nil
 }
-func (s *server) GetAllFiles(req *gigxRR.GetAllFilesRequest, stream gigxRR.GigxRRService_GetAllFilesServer)error {
+func (s *server) GetAllFiles(req *gigxRR.GetAllFilesRequest, stream gigxRR.GigxRRService_GetAllFilesServer) error {
 	return nil
 }
 func (s *server) UpdateFile(ctx context.Context, req *gigxRR.UpdateFileRequest) (*gigxRR.UpdateFileResponse, error) {
-	return nil,nil
+	return nil, nil
 }
 func (s *server) DeleteFile(ctx context.Context, req *gigxRR.DeleteFileRequest) (*gigxRR.DeleteFileResponse, error) {
-	return nil,nil
+	return nil, nil
+}
+func (s *server) UploadFile(stream gigxRR.GigxRRService_UploadFileServer) error {
+	fmt.Printf("RR service is working for UploadFile...Received rpc from client.\n")
+	return cont.UploadFileController(stream)
 }
 
-func main(){
+func main() {
 	pflag.Parse()
 
-	port :=8900
+	port := 8900
 	if *enableTls {
 		port = 8901
 	}
@@ -111,16 +116,17 @@ func main(){
 	c = cache.New(5*time.Minute, 10*time.Minute)
 
 	opts := []grpc.ServerOption{}
+	opts = append(opts, grpc.MaxRecvMsgSize(1024*1024*1024))
 	grpcServer := grpc.NewServer(opts...)
 	gigxRR.RegisterGigxRRServiceServer(grpcServer, &server{})
 
 	fmt.Println("Mongodb Service Started")
-	if confErr:=repo.LoadConfiguration("dev"); confErr!="ok"{
+	if confErr := repo.LoadConfiguration("dev"); confErr != "ok" {
 		fmt.Println(confErr)
 	}
 
 	allowedOrigins := helper.MakeAllowedOrigins()
-	
+
 	options := []grpcweb.Option{
 		grpcweb.WithCorsForRegisteredEndpointsOnly(false),
 		grpcweb.WithOriginFunc(helper.MakeHttpOriginFunc(allowedOrigins)),
@@ -138,14 +144,13 @@ func main(){
 
 	grpclog.Printf("Starting server. http port: %d, with TLS: %v", port, *enableTls)
 
-
 	if *enableTls {
-		fmt.Printf("server started as  https and listen to port: %v \n",port)
+		fmt.Printf("server started as  https and listen to port: %v \n", port)
 		if err := httpServer.ListenAndServeTLS(*tlsCertFilePath, *tlsKeyFilePath); err != nil {
 			grpclog.Fatalf("failed starting http2 server: %v", err)
 		}
 	} else {
-		fmt.Printf("server started as http and listen to port: %v \n",port)
+		fmt.Printf("server started as http and listen to port: %v \n", port)
 		if err := httpServer.ListenAndServe(); err != nil {
 			grpclog.Fatalf("failed starting http server: %v", err)
 		}
